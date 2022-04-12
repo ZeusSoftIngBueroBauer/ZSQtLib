ab80cb3def67a7638667f711579aceae1327aee1
Christian Bauer <Christian.Bauer@ZeusSoft.de>
2022-04-12 23:18:12 +0200

    MutexLocker in ZSIpcTrace not traced if created in thread after doTestStep returned (setNewTrcAdminObjsDefaultDetailLevel).

--- Source/Apps/Test/ZSTemplate/Main.cpp
+++ Source/Apps/Test/ZSTemplate/Main.cpp
@@ -39,14 +39,6 @@ using namespace ZS::Apps::Test::Template;
 
 
 /*******************************************************************************
-#pragma message(__FILE__ ": Linking against = " ZSIPCLIB)
-#pragma comment(lib, ZSIPCLIB)
-#pragma message(__FILE__ ": Linking against = " ZSIPCGUILIB)
-#pragma comment(lib, ZSIPCGUILIB)
-#pragma message(__FILE__ ": Linking against = " ZSIPCTRACELIB)
-#pragma comment(lib, ZSIPCTRACELIB)
-#pragma message(__FILE__ ": Linking against = " ZSIPCTRACEGUILIB)
-#pragma comment(lib, ZSIPCTRACEGUILIB)
 Entry point for the application.
 *******************************************************************************/
 
